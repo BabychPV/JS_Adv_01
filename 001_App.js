cb99218afc16a0e40a81244fba87be4b7a2e7f7a
Babych <babych.p.v@gmail.com>
2020-10-28 22:19:27 +0200

    5

--- 001_App.js
+++ 001_App.js
@@ -6,33 +6,30 @@ function Box(w, h, d, m) {
 }
 
 Box.prototype.Volume = function () {
-    return this.width * this.height* this.depth;
+    return this.width * this.height * this.depth;
 }
 
 
 Box.prototype.toString = function () {
-    return "Коробка M:"  + this.material  + " W:" + this.width + " H:" + this.height + "  D:" + this.depth + '<br>';
-}
+    return "Коробка M:" + this.material + " W:" + this.width + " H:" + this.height + "  D:" + this.depth + '<br>';
+};
 
 
 // метод для проверки равенства объектов
 Box.prototype.equals = function (otherObj) {
-    if ((this.width === otherObj.width && this.height === otherObj.height && this.depth === otherObj.depth) && (this.material === otherObj.material)) {
-        return true;
-    }
-    return false;
-}
+    return (this.width === otherObj.width && this.height === otherObj.height && this.depth === otherObj.depth) && (this.material === otherObj.material);
+};
 
 
 let Box1 = new Box(100, 50, 50, 'Box1');
 let Box2 = new Box(100, 50, 50, 'Box2');
 let Box3 = new Box(100, 40, 50, 'Box3');
 
-document.write(Box1)
+document.write(Box1.toString());
 document.write("Объем коробки 1 " + Box1.Volume() + "<br />");
-document.write(Box2)
+document.write(Box2.toString());
 document.write("Объем коробки 2 " + Box2.Volume() + "<br />");
-document.write(Box3)
+document.write(Box3.toString());
 document.write("Объем коробки 2 " + Box3.Volume() + "<br />");
 
 
